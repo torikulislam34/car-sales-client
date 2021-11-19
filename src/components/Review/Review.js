03f1a0351a61c9d5f7de330b2cc06b2ce6c8fda6
torikulislam34 <leotorikul@gmail.com>
2021-11-19 23:23:33 +0600

    reting update

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './review.css';
 import { Link } from 'react-router-dom';
+import Rating from 'react-rating';
 
 const Review = (props) => {
     const {title,img,reting,dis,_id} = props.review; 
@@ -12,6 +13,10 @@ const Review = (props) => {
                <img src={img} className="card-img-top review-img" alt="..."/>
                <div className="card-body">
                <h5 className="card-title fw-bold" style={{color:'brown'}}>Name: {title}</h5>
+               <Rating initialRating ={reting}
+              readonly
+              emptySymbol ="far fa-star text-warning"
+              fullSymbol ="fas fa-star text-warning"></Rating>
                <p className='text-center'><span className='fw-bold '>Feedback: </span> {dis.slice(0,90)}.</p>
                </div>
              </div>
